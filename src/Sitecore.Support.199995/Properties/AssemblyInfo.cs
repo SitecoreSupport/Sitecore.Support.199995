6c4937c0ddc022287d3fa20783418a7fa6b71165
Alexey Tupkalo <altu@sitecore.net>
2017-12-27 18:29:42 +0200

    Merge 77381, 95002, 101295, 140555

---
+++ src/Sitecore.Support.199995/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.199995")]
+[assembly: AssemblyProduct("Sitecore.Support.199995")]
+[assembly: ComVisible(false)]
